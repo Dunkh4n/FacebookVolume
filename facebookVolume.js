ed5ffa2c02b39977a8f1c8d0ea0e5ca1e2c52f38
Greg <Dunkh4n@users.noreply.github.com>
2020-05-02 16:17:53 +0200

    change github url

--- facebookVolume.js
+++ facebookVolume.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         FacebookVolume
 // @description  A tampermonkey script to manage facebook volume video
+// @homepage     https://github.com/Dunkh4n/FacebookVolume
 // @namespace    https://www.facebook.com/
 // @author       Dunkh4n
 // @version      0.1
-// @updateURL    https://github.com/Dunkh4n/FacebookVolume
-// @downloadURL  https://github.com/Dunkh4n/FacebookVolume
+// @updateURL    https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
+// @downloadURL  https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
 // @match        https://www.facebook.com/*
 // @run-at       document-end
 // ==/UserScript==
