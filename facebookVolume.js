7c61a735dbe657d3aefd536e67f9f207c9542b0a
Greg <Dunkh4n@users.noreply.github.com>
2020-05-02 16:14:41 +0200

    first commit

---
+++ facebookVolume.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         FacebookVolume
+// @description  A tampermonkey script to manage facebook volume video
+// @namespace    https://www.facebook.com/
+// @author       Dunkh4n
+// @version      0.1
+// @updateURL    https://github.com/Dunkh4n/FacebookVolume
+// @downloadURL  https://github.com/Dunkh4n/FacebookVolume
+// @match        https://www.facebook.com/*
+// @run-at       document-end
+// ==/UserScript==
