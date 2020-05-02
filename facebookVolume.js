76171d3b13c65171c07da8dc2df1a96b59db2c8e
Greg <Dunkh4n@users.noreply.github.com>
2020-05-02 16:24:07 +0200

    version change to chek tampermonkey auto update

--- facebookVolume.js
+++ facebookVolume.js
@@ -4,9 +4,9 @@
 // @homepage     https://github.com/Dunkh4n/FacebookVolume
 // @namespace    https://www.facebook.com/
 // @author       Dunkh4n
-// @version      0.1.1
-// @updateURL    https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
-// @downloadURL  https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
+// @version      0.2
+// @updateURL    https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/dev/facebookVolume.js
+// @downloadURL  https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/dev/facebookVolume.js
 // @match        https://www.facebook.com/*
 // @run-at       document-end
 // ==/UserScript==
