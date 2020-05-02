7b45e2550d3f751183d430393ac6335807262b4a
Greg <Dunkh4n@users.noreply.github.com>
2020-05-02 16:21:12 +0200

    first commit to dev branch

--- facebookVolume.js
+++ facebookVolume.js
@@ -4,9 +4,16 @@
 // @homepage     https://github.com/Dunkh4n/FacebookVolume
 // @namespace    https://www.facebook.com/
 // @author       Dunkh4n
-// @version      0.1
+// @version      0.1.1
 // @updateURL    https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
 // @downloadURL  https://raw.githubusercontent.com/Dunkh4n/FacebookVolume/master/facebookVolume.js
 // @match        https://www.facebook.com/*
 // @run-at       document-end
 // ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Your code here...
+
+})();
