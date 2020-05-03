a031a786a7a4941bdbafac4938ac42bb9f6d871e
Greg <Dunkh4n@users.noreply.github.com>
2020-05-03 22:45:01 +0200

    Working

--- facebookVolume.js
+++ facebookVolume.js
@@ -14,6 +14,84 @@
 (function () {
   'use strict';
 
-  // Your code here...
+  let observer = new MutationObserver(startObserver);
+  let obsrvConfig = {
+    childList: true,
+    attributes: true,
+    subtree: true,
+    characterData: true,
+  };
+  let mainContent = document.getElementById('content');
+  let videoList = mainContent.getElementsByTagName('video');
+  let videoListArr = [];
+  const MUTEDDEFAULT = false;
+  const VOLUMEDEFAULT = 0.1;
+  const VOLUMEDEFAULTBYSESION = false;
+  let newVolume = 0;
+  const DEBUG = 1;
 
+  function videoHandler(video) {
+    if (!video.hasAttribute('data-fbvID')) {
+      let videoHashID = hash(video.getAttribute('src'));
+      console.log('videoHashID : ', videoHashID);
+
+      video.setAttribute('data-fbvID', videoHashID);
+      video.muted = MUTEDDEFAULT;
+      video.volume = VOLUMEDEFAULT;
+      video.addEventListener('volumechange', volumeChange);
+      videoListArr.push(video);
+    }
+
+    if (DEBUG) {
+      console.log('video found : ', video);
+      console.log('videoListArr : ', videoListArr);
+      console.log('videoList length : ', videoListArr.length);
+      console.log('##########################');
+    }
+  }
+
+  function volumeChange(event) {
+    if (DEBUG) {
+      console.log('The volume changed : ', event.target.volume);
+    }
+    newVolume = event.target.volume;
+    Array.from(videoList).forEach((video) => {
+      video.volume = newVolume;
+    });
+  }
+
+  function startObserver(mutations) {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((video) => {
+        if (DEBUG) {
+          // console.log(
+          //   'addedNode.querySelectorAll("video") : ',
+          //   video.querySelectorAll('video')
+          // );
+        }
+        Array.from(videoList).forEach(videoHandler);
+      });
+    });
+  }
+
+  function hash(s) {
+    /* Simple hash function. */
+    var a = 1,
+      c = 0,
+      h,
+      o;
+    if (s) {
+      a = 0;
+      /*jshint plusplus:false bitwise:false*/
+      for (h = s.length - 1; h >= 0; h--) {
+        o = s.charCodeAt(h);
+        a = ((a << 6) & 268435455) + o + (o << 14);
+        c = a & 266338304;
+        a = c !== 0 ? a ^ (c >> 21) : a;
+      }
+    }
+    return String(a);
+  }
+
+  observer.observe(mainContent, obsrvConfig);
 })();
